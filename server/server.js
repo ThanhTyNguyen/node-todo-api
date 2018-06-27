d4a8d4bd4c9fd1f8f6b02d55dd5761211049f441
ThanhTy <developnguyen@gmail.com>
2018-06-27 23:10:15 +0800

    modified in app.listen()

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`server is running at 3000 ${port}`);
+    console.log(`server is running at ${port}`);
 });
 
 module.exports = {app};
