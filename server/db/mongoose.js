89b75d4fa6efaef85f934500abf2489b1fdc4934
ThanhTy <developnguyen@gmail.com>
2018-06-29 01:36:45 +0800

    add Schema, instance method in model, and POST /users

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI); // this will be called whenever query task are ran from server.js
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 require('./config/config');
-
+require('./db/mongoose');
 const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 const bodyParser = require('body-parser');
 const express = require('express');
 
-var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -13,7 +12,6 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -91,6 +89,21 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(404).send(e);
+    })
 });
 
 app.listen(port, () => {
